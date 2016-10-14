173b2503da3988400c455dd1c446e414901e6c44
arabali <cmsakthi@gmail.com>
2016-10-14 22:41:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'sa.html'));
 });
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui ', 'article1.html'));
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
